86dad153383b231e3c706fe1e19a7ec20e83d82e
Bema Konate <professorkonate@gmail.com>
2019-10-02 19:34:39 -0400

    Add project to git

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import React, { Component } from 'react';
+import './App.css';
+
+class App extends Component {
+  render() {
+    return (
+      <div className="App">
+        <h1>Welcome to <a>sass</a> react setup.</h1>
+        <p>Write files in scss file format then when app start css file will be made or updated.</p>
+      </div>
+    );
+  }
+}
+
+export default App;
