5a9bc771d44c713bd8c871a92f3bd734c64ac97d
Bema Konate <professorkonate@gmail.com>
2019-10-03 20:46:54 -0400

    Add css modules

--- src/containers/App/App.js
+++ src/containers/App/App.js
@@ -2,11 +2,12 @@ import React, { Component } from 'react';
 import './App.css';
 import HomeBuilder from '../HomeBuilder/HomeBuilder';
 import Layout from '../Layout/Layout';
+import classes from './App.css'
 
 class App extends Component {
   render() {
     return (
-      <div className="App">
+      <div className={classes.App}>
         <Layout>
           <HomeBuilder />
         </Layout>
