86eeec495002f129ef0cf31616b96ba9b7f0c6b1
Bema Konate <professorkonate@gmail.com>
2019-10-05 16:26:02 -0400

    Change background image

--- src/components/ToDoList/ToDoList.js
+++ src/components/ToDoList/ToDoList.js
@@ -66,8 +66,7 @@ class ToDoList extends Component {
             <div className={classes.ToDoList}>
                 <div className={classes.TodoHeader}>
                     <h3 className={classes.TodoTitle}>Todo List App</h3>
-                    <p className={classes.Tracker}>Task Completed: {this.state.tasksCompleted}</p>
-
+                    <p className={classes.Tracker}>Task(s) Completed: {this.state.tasksCompleted}</p>
                 </div>
                 <ToDoInput changed={this.inputChangedHandler} />
                 <div className={classes.Todos}>
